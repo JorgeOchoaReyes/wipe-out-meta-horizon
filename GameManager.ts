b71ec1ef57abcc0ee8b5555fbccd53c216032e37
Jorge Raul Ochoa-Reyes <jorgeochoareyes@gmail.com>
2025-04-24 00:00:17 -0700

    spin platform

--- GameManager.ts
+++ GameManager.ts
@@ -1,5 +1,4 @@
-import * as hz from 'horizon/core';
-import { Entity } from 'horizon/core';
+import * as hz from 'horizon/core'; 
 
 class GameManager extends hz.Component<typeof GameManager> {
   static propsDefinition = {
@@ -8,12 +7,7 @@ class GameManager extends hz.Component<typeof GameManager> {
   };
 
   start() {
-    this.connectCodeBlockEvent(this.entity, hz.CodeBlockEvents.OnPlayerEnterWorld, (player: hz.Player) => {
-      const backgroundMusicGizmo = this.props.backgroundMusic!.as(hz.AudioGizmo)!;
-      backgroundMusicGizmo.play({ fade: 0, players: [player] });
-      backgroundMusicGizmo.volume.set(0.5); // Optional: Set the volume to 50%
-      // how to loop? 
-    });
+ 
   }
 }
 hz.Component.register(GameManager); 
