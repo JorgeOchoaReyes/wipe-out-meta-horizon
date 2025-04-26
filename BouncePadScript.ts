2bd0d25a69dc8a1db6f7a609582caba151ee7c92
Jorge Raul Ochoa-Reyes <jorgeochoareyes@gmail.com>
2025-04-25 17:06:02 -0700

    added ball and chain scripts and prefabs

--- BouncePadScript.ts
+++ BouncePadScript.ts
@@ -4,34 +4,39 @@ import { PropTypes, Entity, Player, CodeBlockEvents, Vec3 } from 'horizon/core';
 class BouncePadScript extends hz.Component<typeof BouncePadScript> {
   static propsDefinition = {
     bounceForce: { type: PropTypes.Number, default: 10 },
-    playerDetectionRadius: { type: PropTypes.Number, default: 1 }
+    playerDetectionRadius: { type: PropTypes.Number, default: 1 },
+    soundFx: { type: PropTypes.Entity},
   };
   
   private bounceForce!: number;
-  private playerDetectionRadius!: number;
+  private playerDetectionRadius!: number; 
 
   start() {
     this.bounceForce = this.props.bounceForce!;
-    this.playerDetectionRadius = this.props.playerDetectionRadius!;
-
+    this.playerDetectionRadius = this.props.playerDetectionRadius!; 
     this.connectCodeBlockEvent(this.entity, CodeBlockEvents.OnPlayerEnterTrigger, (player: Player) => {
       this.onPlayerLanded(player);
     }); 
   }
 
   onPlayerLanded(player: Player) { 
+    console.log("Player landed on bounce pad");
     const playerPosition = player.position.get();
     const platformPosition = this.entity.position.get();
     const distance = playerPosition.sub(platformPosition).magnitude();
+    const direction = playerPosition.sub(platformPosition).normalize();
+    const force = direction.mul(this.bounceForce)
 
-    if (distance < this.playerDetectionRadius) {
-      const bounceDirection = new Vec3(0, 1, 0);  
-      const bounceVelocity = bounceDirection.mul(this.bounceForce);
-      player.velocity.set(bounceVelocity);
+    if (distance < this.playerDetectionRadius) {   
+      player.applyForce(force);
+      if(this.props.soundFx) { 
+        this.props.soundFx.as(hz.AudioGizmo).play({
+          fade: 0,
+          players: [player],
+        })
+      }
     }
   }
- 
-
 }
 hz.Component.register(BouncePadScript);
  
--- GameManager.ts
+++ GameManager.ts
@@ -1,13 +1,34 @@
 import * as hz from 'horizon/core'; 
 
+export const playerCheckpoints = new hz.LocalEvent<Map<hz.Player, hz.SpawnPointGizmo>>('playerCheckpoints');
+export const setPlayerCheckpoints = new hz.LocalEvent<Map<hz.Player, hz.SpawnPointGizmo>>('setPlayerCheckpoints');
+
 class GameManager extends hz.Component<typeof GameManager> {
-  static propsDefinition = {
-    backgroundMusic: { type: hz.PropTypes.Entity },
-    checkpoint: { type: hz.PropTypes.Entity },
+  static propsDefinition = { 
+    checkpoint1: { type: hz.PropTypes.Entity },
+    checkpoint2: { type: hz.PropTypes.Entity },
+    checkpoint3: { type: hz.PropTypes.Entity },
+    checkpoint4: { type: hz.PropTypes.Entity },
+    checkpoint5: { type: hz.PropTypes.Entity },
   };
 
+  private playerCheckpoints: Map<hz.Player, hz.SpawnPointGizmo> = new Map();
+
   start() {
- 
+    
+    this.connectLocalBroadcastEvent(setPlayerCheckpoints, (data) => { 
+      this.playerCheckpoints = data;
+    }); 
+
+    this.connectCodeBlockEvent(this.entity, hz.CodeBlockEvents.OnPlayerEnterWorld, (player: hz.Player) => {
+      this.setPlayerCheckpoint(player, this.props.checkpoint1?.as(hz.SpawnPointGizmo)!);
+    }); 
   }
+
+  setPlayerCheckpoint(player: hz.Player, checkpoint: hz.SpawnPointGizmo) {
+    this.playerCheckpoints.set(player, checkpoint);
+    this.sendLocalBroadcastEvent(playerCheckpoints, this.playerCheckpoints);
+  }
+  
 }
 hz.Component.register(GameManager); 
